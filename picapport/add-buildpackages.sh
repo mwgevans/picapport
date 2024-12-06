6409f1c0c2137ecd2f335b9516f10e028ad0d6ad
Mark Evans <mark@riotenterprises.com>
2024-12-06 15:16:14 +0000

    Initial Build

---
+++ picapport/add-buildpackages.sh
@@ -0,0 +1,8 @@
+apk upgrade && \
+apk update && \
+apk add --no-cache musl-dev \
+        gcc \
+        libjpeg-turbo-dev \
+        lcms2-dev \
+        jasper-dev \
+        curl;
---
+++ picapport/build-dcraw.sh
@@ -0,0 +1,4 @@
+mkdir -p /opt/dcraw/src
+(cd /opt/dcraw/src && curl -O https://dechifro.org/dcraw/dcraw.c )
+mkdir -p /opt/dcraw/bin;
+gcc -o /opt/dcraw/bin/dcraw -O4 /opt/dcraw/src/dcraw.c -lm -ljasper -ljpeg -llcms2
