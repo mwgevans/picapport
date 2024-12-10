95999fd630df4efe2f538140d5874ace2bb3c4b4
Mark Evans <mark@riotenterprises.com>
2024-12-10 13:56:31 +0000

    fix plugin copy

--- picapport/start-picapport.sh
+++ picapport/start-picapport.sh
@@ -3,7 +3,7 @@ if [ ! -e /opt/picapport/.picapport/picapport.properties ]; then
     # create picapport.properties
     printf "%s\n%s\n%s\n%s\n%s\n%s\n" "server.port=8080" "robot.root.0.path=/srv/photos" "client.theme.darktheme=true" "client.theme.color=#34568B" "foto.jpg.usecache=2" "foto.jpg.cache.path=/srv/cache" "plugin.shadow.path=/srv/picapportshadowfiles"> /opt/picapport/.picapport/picapport.properties
     cp -R /opt/picapportgroovy ./.picapport/groovy
-    cp -R /opt/picapportplugins ./.picapport/plugins
+    cp -R /opt/picapportplugins/* ./.picapport/plugins/
 fi
-java "-Xms$XMS" "-Xmx$XMX" -DTRACE=DEBUG -Duser.home=/opt/picapport -jar picapport-headless.jar
+java "-Xms$XMS" "-Xmx$XMX" "-DTRACE=$DTRACE" -Duser.home=/opt/picapport -jar picapport-headless.jar
 
